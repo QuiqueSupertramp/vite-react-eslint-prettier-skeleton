d2a57bb6de90c0f0fbf867772478beb763457910
Quique Supertramp <quiquesupertramp@gmail.com>
2022-04-08 15:04:54 +0200

    skeleton

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -6,7 +6,7 @@ import App from './App';
 const container = document.getElementById('root');
 
 createRoot(container).render(
-	<React.StrictMode>
+	<StrictMode>
 		<App />
-	</React.StrictMode>
+	</StrictMode>
 );
