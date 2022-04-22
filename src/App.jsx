05e1a42555093c16819c1a539d3af5a33acdf03d
Quique Supertramp <quiquesupertramp@gmail.com>
2022-04-22 02:49:26 +0200

    add BASE URL with @

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import './App.css';
 
 function App() {
-	return <div className='App'></div>;
+	return (
+		<div className='App'>
+			<h1>ESQUELETO VITE + REACT</h1>
+			<ul>
+				<li>ESLINT</li>
+				<li>PRETTIER</li>
+				<li>IMPORTS ABSOLUTOS CON @</li>
+			</ul>
+		</div>
+	);
 }
 
 export default App;
